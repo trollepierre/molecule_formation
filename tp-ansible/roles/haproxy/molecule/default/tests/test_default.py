7f6f8461d78141c5d1b729755f7e4e8f005def8f
OCTO <octo@octo.com>
2018-11-16 15:36:32 +0100

    add test on molecule

--- tp-ansible/roles/haproxy/molecule/default/tests/test_default.py
+++ tp-ansible/roles/haproxy/molecule/default/tests/test_default.py
@@ -11,3 +11,23 @@ def test_haproxy_config_file(host):
 
     assert f.exists
     assert f.contains('server  vm_tango_02  127.0.0.1:8080 check')
+
+
+def test_haproxy_is_installed(host):
+    haproxy = host.package("haproxy")
+    assert haproxy.is_installed
+
+
+def test_haproxy_is_on_good_version(host):
+    haproxy = host.package("haproxy")
+    assert haproxy.version.startswith("1.4")
+
+
+def test_server_is_started(host):
+    haproxy = host.service("haproxy")
+    assert haproxy.is_running
+
+
+def test_server_is_enabled(host):
+    haproxy = host.service("haproxy")
+    assert haproxy.is_enabled
